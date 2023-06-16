a69c64cc70dc1f0c239bfac57333301929dd0ee9
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2023-06-16 16:46:54 -0500

    Move the Material-UI theme into it's own file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 
 import Home from "./pages/Home/Home.jsx";
 import About from "./pages/About/About.jsx";
 import Navigation from "./components/Navigation/Navigation.jsx";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      light: "#679fc9",
-      main: "#4188bc",
-      dark: "#2d5f83",
-      contrastText: "#fff",
-    },
-    secondary: {
-      light: "#e0e0e0",
-      main: "#d9d9d9",
-      dark: "#979797",
-      contrastText: "#000",
-    },
-  },
-  components: {
-    MuiOutlinedInput: {
-      styleOverrides: {
-        root: {
-          color: '#d9d9d9', // Set input text color
-          '& .MuiOutlinedInput-notchedOutline': {
-            borderColor: '#d9d9d9', // Border color
-          },
-          '&:hover .MuiOutlinedInput-notchedOutline': {
-            borderColor: '#d9d9d9', // Border color on hover
-          },
-        },
-      },
-    },
-    MuiFormLabel: {
-      styleOverrides: {
-        root: {
-          color: '#d9d9d9', // Label color
-          '&.Mui-focused': {
-            color: '#d9d9d9', // Label color when focused
-          },
-        },
-      },
-    },
-  },
-});
-
+import theme from "./theme";
 
 function App() {
   return (
---
+++ frontend/src/theme.js
@@ -0,0 +1,45 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        light: "#679fc9",
+        main: "#4188bc",
+        dark: "#2d5f83",
+        contrastText: "#fff",
+      },
+      secondary: {
+        light: "#e0e0e0",
+        main: "#d9d9d9",
+        dark: "#979797",
+        contrastText: "#000",
+      },
+    },
+    components: {
+      MuiOutlinedInput: {
+        styleOverrides: {
+          root: {
+            color: '#d9d9d9', // Set input text color
+            '& .MuiOutlinedInput-notchedOutline': {
+              borderColor: '#d9d9d9', // Border color
+            },
+            '&:hover .MuiOutlinedInput-notchedOutline': {
+              borderColor: '#d9d9d9', // Border color on hover
+            },
+          },
+        },
+      },
+      MuiFormLabel: {
+        styleOverrides: {
+          root: {
+            color: '#d9d9d9', // Label color
+            '&.Mui-focused': {
+              color: '#d9d9d9', // Label color when focused
+            },
+          },
+        },
+      },
+    },
+  });
+
+export default theme;
