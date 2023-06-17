fa51848546aa0f17844f8d45f9bf2bd8f94fe363
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2023-06-17 12:09:23 -0500

    update app.py to include CORS and not use envs

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,12 @@
-# /backend/app.py
-import os
-
 from flask import Flask
-from dotenv import load_dotenv
+from flask_cors import CORS
 from routes import gpt_routes, dalle_routes
 
-# Load environment variables
-load_dotenv()
 
 # Initialize Flask application
 app = Flask(__name__)
+CORS(app)
 
-# Set configuration
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
-app.config['DALLE_API_KEY'] = os.getenv('DALLE_API_KEY')
 
 # Register blueprints
 app.register_blueprint(gpt_routes.bp)
