1ef2e99174226d2b003cbd7308503aa8cbaf1045
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2023-06-18 09:58:38 -0500

    Modify LoadingImage Component to display more information

--- frontend/src/components/Loading/LoadingImage.jsx
+++ frontend/src/components/Loading/LoadingImage.jsx
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import CardContent from "@mui/material/CardContent";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
 import Typography from "@mui/material/Typography";
 
+function LoadingImage({ ingredients, recipeNameInput }) {
+  const [countdown, setCountdown] = useState(15);
 
-function LoadingImage({ loadingText }) {
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCountdown((prevCountdown) => prevCountdown - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  // Extract the names of ingredients
+  const ingredientNames = ingredients.map((ingredient) => ingredient.name);
+
+  // Construct the recipe text
+  const recipeText = `A nice image of a delicious meal called ${recipeNameInput} made from ${ingredientNames.join(
+    ", "
+  )}`;
   return (
     <CardContent
-          sx={{
-            backgroundColor: "#FFFFFF",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            minHeight: 200,
-            minWidth: 400,
-          }}
-        >
-          <CircularProgress /> 
-          <Typography variant="h6" fontWeight="bold" color="text.secondary">
-            Image Is loading
-          </Typography>
-        </CardContent>
+      sx={{
+        backgroundColor: "#FFFFFF",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: 200,
+        minWidth: 400,
+      }}
+    >
+      <Box sx={{ marginBottom: 2 }}>
+        <CircularProgress />
+      </Box>
+      <Typography
+        variant="h6"
+        fontWeight="bold"
+        color="text.secondary"
+        textAlign="center"
+      >
+        Image Is loading <br />
+        The prompt being sent to DALL-E API is: <br />
+        <span style={{ color: "black" }}>
+          {recipeText} <br />
+        </span>{" "}
+        <br />
+        This could take up to 15 seconds <br />
+        {countdown} <br />
+        {countdown <= 0 &&
+          countdown > -10 &&
+          "It is still loading.....at least I have chicken.."}
+        {countdown <= -10 &&
+          "Oh no. Right click on the web page and click inspect. Then select the console tab. Paste your error in ChatGPT"}
+      </Typography>
+    </CardContent>
   );
 }
 
