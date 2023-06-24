e82c7c9febfe001cce1816c52b0f5140e3478c18
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2023-06-24 16:38:18 -0500

    Create a seperate Ingredient Chips component

---
+++ frontend/src/components/IngredientChips.jsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Chip from "@mui/material/Chip";
+
+function IngredientChips({ ingredients }) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        listStyle: "none",
+        padding: 1,
+        margin: 0,
+      }}
+    >
+      {ingredients.map((ingredient, index) => (
+        <Box key={index} sx={{ margin: 1 }}>
+          <Chip
+            label={`${ingredient.name} - ${ingredient.amount} ${ingredient.unit}`}
+            sx={{
+              fontWeight: "bold",
+              fontFamily: "Arial",
+            }}
+          />
+        </Box>
+      ))}
+    </Box>
+  );
+}
+
+export default IngredientChips;
