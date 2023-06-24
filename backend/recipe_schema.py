b73b0dcc3125ac17ec969b362fcabc98253f8f68
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2023-06-24 16:35:47 -0500

    Update the schema to ask for ChatGPT to generate the dalle prompt

--- backend/recipe_schema.py
+++ backend/recipe_schema.py
@@ -9,29 +9,36 @@ schema = {
             "items": {
                 "type": "object",
                 "properties": {
-                    "name": { "type": "string" },
-                    "unit": { 
+                    "name": {"type": "string"},
+                    "unit": {
                         "type": "string",
-                        "enum": ["grams", "ml", "cups", "pieces", "teaspoons"]
+                        "enum": ["grams", "ml", "cups", "pieces", "teaspoons"],
                     },
-                    "amount": { "type": "number" }
+                    "amount": {"type": "number"},
                 },
-                "required": ["name", "unit", "amount"]
-            }
+                "required": ["name", "unit", "amount"],
+            },
         },
         "instructions": {
             "type": "array",
             "description": "Steps to prepare the recipe (no numbering)",
-            "items": { "type": "string" }
+            "items": {"type": "string"},
         },
         "time_to_cook": {
             "type": "number",
-            "description": "Total time to prepare the recipe in minutes"
+            "description": "Total time to prepare the recipe in minutes",
         },
-        "serious_to_silly_rating": {
-            "type": "number",
-            "description": "On a scale of 1 to 10, 1 being a legitimate and a serious recipe name, and 10 being really silly and definitely not food, what would you rate this recipe?",
-        }
+        "visual_discription_of_image": {
+            "type": "string",
+            "description": "A prompt to Dalle to get the most accurate depiction of this item, please base it off of the ingredients and instructions, Get very detailed up to 300 characters",
+        },
     },
-    "required": ["ingredients", "instructions", "time_to_cook"]
+    "required": ["ingredients", "instructions", "time_to_cook"],
 }
+
+# Here is an example of improvement in prompt engineering, I was trying to have ChatGPT rate the silliness of the recipe name so I could conditionally
+# build a prompt, then I realized that I could just have ChatGPT build the prompt for me!
+# "serious_to_silly_rating": {
+#     "type": "number",
+#     "description": "On a scale of 1 to 10, 1 being a legitimate and a serious recipe name, and 10 being really silly and definitely not food, what would you rate this recipe?",
+# },
