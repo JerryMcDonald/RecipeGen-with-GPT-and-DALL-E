4a34c836baec1d4b9b68ddc42ce16923f69fe302
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2023-06-24 16:39:21 -0500

    Create a seperate Recipe Form component

---
+++ frontend/src/components/RecipeForm.jsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+function RecipeForm({ onGenerateRecipe, recipeNameInput, handleInputChange }) {
+  return (
+    <>
+      <TextField
+        label="Enter recipe name"
+        value={recipeNameInput || ""}
+        onChange={handleInputChange}
+        onKeyUp={(event) => {
+          if (event.key === "Enter") {
+            onGenerateRecipe();
+          }
+        }}
+        variant="outlined"
+        color="secondary"
+        sx={{ marginBottom: 2 }}
+      />
+      <Button variant="contained" color="secondary" onClick={onGenerateRecipe}>
+        Enter
+      </Button>
+    </>
+  );
+}
+
+export default RecipeForm;
